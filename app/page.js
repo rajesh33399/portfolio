01cc9f3ea77e71a38940894b82ff8d8474a496e9
Rajesh Botla <rajeshbotla4@gmail.com>
2026-09-02 11:11:39 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -94,7 +94,7 @@ export default function Home() {
             <TypewriterEffect 
               words={[
                 'Full Stack Software Engineer',
-                'GenAI Engineer'
+                'GenAI Developer'
               ]} 
             />
           </div>
